2489ada383331f4b2e7b4c71c728df9ca4e83207
hulashsaini21 <95952301+hulashsaini21@users.noreply.github.com>
2024-07-28 12:46:26 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -161,12 +161,6 @@ async def account_login(bot: Client, m: Message):
 
             elif 'videos.classplusapp' in url:
              url = requests.get(f'https://api.classplusapp.com/cams/uploader/video/jw-signed-url?url={url}', headers={'x-access-token': 'eyJjb3Vyc2VJZCI6IjQ1NjY4NyIsInR1dG9ySWQiOm51bGwsIm9yZ0lkIjo0ODA2MTksImNhdGVnb3J5SWQiOm51bGx9'}).json()['url']
-
-            elif "apps-s3-jw-prod.utkarshapp.com" in url:
-              headers = {'Host': 'apps-s3-jw-prod.utkarshapp.com', 'x-access-token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjMwNzc1NSIsImRldmljZV90eXBlIjoiMSIsInZlcnNpb25fY29kZSI6IjE3NyIsImljciI6IjEiLCJpYXQiOjE3MjE5ODI5NjEsImV4cCI6MTcyNDE0Mjk2MX0.6c3yElq8fCLUuiYS3K28hNbyySEzeW084YnhakSv0u0', 'user-agent': 'Mobile-Android', 'app-version': '1.4.37.1', 'api-version': '18', 'device-id': '5d0d17ac8b3c9f51', 'device-details': '2848b866799971ca_2848b8667a33216c_SDK-30', 'accept-encoding': 'gzip'}
-              params = (('url', f'{url}'),)
-              response = requests.get('http://apps-s3-jw-prod.utkarshapp.com/cams/uploader/video/jw-signed-url', headers=headers, params=params)
-              url = response.json()['url']
             
             elif '/master.mpd' in url:
              id =  url.split("/")[-2]
